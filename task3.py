10230f15c0dcc53ae139c693538aca51c0a568f9
sasha <am.zvereva@yandex.ru>
2018-03-31 14:37:10 +0300

    done

--- task3.py
+++ task3.py
@@ -1,1 +1,12 @@
+import numpy as np
+import scipy.linalg as linalg
 
+N=100
+A=np.zeros((N,N))
+i,j=np.indices((N,N))
+A[i==j]=1
+A[i==j+1]=1
+A[i==j+2]=1
+B=np.arange(N)
+b=linalg.solve(A,B)
+print(b)
